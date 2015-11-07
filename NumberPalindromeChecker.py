9c01785f1ca7490e0d090ece9b0e01a945a418d5
Susan Bhattarai <susan.bhattarai@bison.howard.edu>
2015-11-07 04:00:11 -0500

    Create NumberPalindromeChecker.py

---
+++ NumberPalindromeChecker.py
@@ -0,0 +1,11 @@
+
+def NumberPalin(num):
+    temp = num
+    rev = 0
+    while num > 0:
+        rem = num % 10
+        rev = rev * 10 + rem
+        num = num // 10
+    return  rev == temp
+
+print NumberPalin(10101)
